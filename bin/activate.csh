0d170249d2fd6cc90dbf67b8da4088e93bb48178
ShingenTakeda <gustakeda@gmail.com>
2024-01-05 07:59:06 -0300

    Change from object id to array
    For easier use

--- bin/activate.csh
+++ bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/gustavo/Desktop/FerramentaModelos"
+setenv VIRTUAL_ENV "/home/gustavo/Desktop/docx_genetator"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -17,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(FerramentaModelos) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(FerramentaModelos) "
+    set prompt = "(docx_genetator) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(docx_genetator) "
 endif
 
 alias pydoc python -m pydoc
--- main.py
+++ main.py
@@ -13,62 +13,67 @@ from fastapi import FastAPI, File, UploadFile
 from starlette.background import BackgroundTask
 from fastapi.middleware.cors import CORSMiddleware
 
-#Run with this: uvicorn main:app --reload
+# Run with this: uvicorn main:app --reload
 
-#Delete temp files in tmp
+# Delete temp files in tmp
+
+
 def deleteTemp():
     files = glob.glob(tmpDir + "*.docx")
     for f in files:
         os.remove(f)
 
-dir =  os.path.dirname(__file__)
+
+dir = os.path.dirname(__file__)
 modelosPath = dir + "/modelos/"
 tmpDir = dir + "/tmp/"
 
+
 def json_to_doc(json):
-      temp_doc = Document()
-      name = ""
-      for i in json.items():
-            match i[1]["type"]:
-                  case "name":
-                        print("Nome do documento: " + i[1]["t"])
-                        name = i[1]["t"]
-                  case "p":
-                        #print("paragraph: " + i[1]["p"])
-                        print("paragraph|ajuda: " + str(i[1]["ajuda"]))
-                        print("paragraph|removivel: " + str(i[1]["removivel"]))
-                        print("paragraph|text:" + i[1]["c"][0]["t"])
-                        temp_doc.add_paragraph(i[1]["c"][0]["t"])
-                  case "i":
-                        print("image: " + i[1]["i"])
-                        print("image|ajuda: " + i[1]["ajuda"])
-                        print("image|removivel: " + str(i[1]["removivel"]))
-                  case "h":
-                        print("heading: " + i[1]["c"][0]["t"])
-                        print("heading|ajuda: " + i[1]["ajuda"])
-                        print("heading|removivel: " + str(i[1]["removivel"]))
-                        temp_doc.add_heading(i[1]["c"][0]["t"], level=int(i[1]["htype"]))
-                  case "t":
-                        print(f"table| [ rows: {i[1]['rows']}|cols: {i[1]['cols']} ]")
-                        print(f"table|len arrays: {len(i[1]['cells'])}")
-                        print(f"table|arrays: {i[1]['cells']}")
-                        table = temp_doc.add_table(rows=i[1]["rows"], cols=i[1]["cols"])
+    temp_doc = Document()
+    name = ""
+    for i in json:
+        match i["type"]:
+            case "name":
+                print("Nome do documento: " + i["t"])
+                name = i["t"]
+            case "p":
+                # print("paragraph: " + i[1]["p"])
+                print("paragraph|ajuda: " + str(i["ajuda"]))
+                print("paragraph|removivel: " + str(i["removivel"]))
+                print("paragraph|text:" + i["c"][0]["t"])
+                temp_doc.add_paragraph(i["c"][0]["t"])
+            case "i":
+                print("image: " + i["i"])
+                print("image|ajuda: " + i["ajuda"])
+                print("image|removivel: " + str(i["removivel"]))
+            case "h":
+                print("heading: " + i["c"][0]["t"])
+                print("heading|ajuda: " + i["ajuda"])
+                print("heading|removivel: " + str(i["removivel"]))
+                temp_doc.add_heading(i["c"][0]["t"], level=int(i["htype"]))
+            case "t":
+                print(f"table| [ rows: {i['rows']}|cols: {i['cols']} ]")
+                print(f"table|len arrays: {len(i['cells'])}")
+                print(f"table|arrays: {i['cells']}")
+                table = temp_doc.add_table(rows=i["rows"], cols=i["cols"])
 
-                        for t in i[1]["cells"]:
-                              if t == i[1]["cells"][0]:
-                                    counter = 0
-                                    for t in i[1]["cells"][0]:
-                                          hdr_cell = table.rows[0].cells
-                                          hdr_cell[counter].text = t
-                                          counter = counter + 1
-                              else:
-                                    cell = table.add_row().cells
-                                    for b in range(len(t)):
-                                          cell[b].text = t[b]
-                        temp_doc.add_paragraph("")
+                for t in i["cells"]:
+                    if t == i["cells"][0]:
+                        counter = 0
+                        for t in i["cells"][0]:
+                            hdr_cell = table.rows[0].cells
+                            hdr_cell[counter].text = t
+                            counter = counter + 1
+                    else:
+                        cell = table.add_row().cells
+                        for b in range(len(t)):
+                            cell[b].text = t[b]
+                temp_doc.add_paragraph("")
 
-      temp_doc.save(f"{tmpDir}{name}.docx")
-      return name
+    temp_doc.save(f"{tmpDir}{name}.docx")
+    return name
+
 
 app = FastAPI()
 
@@ -85,8 +90,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#root should return all models
-#Whoever consuming it should either treat it or smth
+# root should return all models
+# Whoever consuming it should either treat it or smth
+
+
 @app.get("/")
 async def root():
     m = []
@@ -96,9 +103,11 @@ async def root():
             m.append(json.load(fileJson))
     return m
 
+
 @app.get("/JSON/{modelo}")
 async def mandarModeloJSON(modelo):
-    path = os.path.join(dir + "/modelos/", '{arquivo}.json'.format(arquivo = modelo))
+    path = os.path.join(dir + "/modelos/",
+                        '{arquivo}.json'.format(arquivo=modelo))
     print(path)
     if os.path.exists(path):
         fileJSON = open(path)
@@ -106,16 +115,20 @@ async def mandarModeloJSON(modelo):
     else:
         return {"ERROR": "Arquivo não existe!"}
 
+
 @app.post("/docx")
-async def mandarModeloDocx(payload : Request):
+async def mandarModeloDocx(payload: Request):
     jd = json.loads(await payload.body())
     print(jd)
     name = json_to_doc(jd)
-    return FileResponse(f"{tmpDir}{name}.docx", background=BackgroundTask(deleteTemp))#{"Receiced" : await payload.body()}#
+    # {"Receiced" : await payload.body()}#
+    return FileResponse(f"{tmpDir}{name}.docx", background=BackgroundTask(deleteTemp))
+
 
 @app.post("/docx/{modelo}")
 async def mockDocx(modelo):
-    fileData = open(os.path.join(dir + "/modelos/", '{arquivo}.json'.format(arquivo = modelo)))
+    fileData = open(os.path.join(dir + "/modelos/",
+                    '{arquivo}.json'.format(arquivo=modelo)))
     jd = json.load(fileData)
     json_to_doc(jd)
     return FileResponse(f"{tmpDir}{modelo}.docx", background=BackgroundTask(deleteTemp))
